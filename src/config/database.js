adfcbc26692275818e0cd924cc6002123194fe89
talhiareikdaltech <c0rdeir0.michelle13@gmail.com>
2026-05-22 08:59:18 -0300

    feat:conexao

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,20 @@
-const { Sequelize } = require('sequelize');
-require("dotenv").config();
+const mysql = require('mysql2');
+const dbConfig = require('../../db.json');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host:process.env.DB_HOST,
-        port:process.env.DB_PORT,
-        dialect:process.env.DB_DIALECT || 'mysql',
-        logging:false
-    }
-);
+const connection = mysql.createConnection({
+  host: dbConfig.host,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database
+});
 
-module.exports = sequelize;
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar:', err);
+    return;
+  }
+
+  console.log('Conectado ao MySQL!');
+});
+
+module.exports = connection;
